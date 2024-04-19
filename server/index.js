c8a01b93bd622eb51e085599ceacbd340e0676d3
Shashi Hans shashi.hans@coverstack.in <shashi.hans@coverstack.in>
2024-04-19 11:04:51 +0530

    min

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require('./config/db');
 const createError = require("http-errors");
+
+
+// create express app
 const app = express();
 
 // Required Route
